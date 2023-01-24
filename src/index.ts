f6d89863133293fe4de5803d140569504cfc2750
Sam Barros <sam.barros@flexcavo.de>
2023-01-24 14:36:52 +0100

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,9 @@
 import * as core from "@actions/core";
-import { context, getOctokit } from "@actions/github";
-import { GitHub } from "@actions/github/lib/utils";
-
-class GitHubPullRequest {
-  private octokit: InstanceType<typeof GitHub>;
-  private target: {
-    [key: string]: any;
-    number: number;
-    html_url?: string;
-    body?: string;
-  };
-
-  constructor(token: string) {
-    this.octokit = getOctokit(token);
-
-    const pullRequest = context.payload.pull_request;
-    const issue = context.payload.issue;
-    const target = pullRequest || issue;
-    if (target === undefined) {
-      throw new Error("It's not possible to assign the user");
-    }
-    this.target = target;
-  }
-
-  async validate() {
-    const {
-      assignees,
-      user: { type },
-    } = this.target;
-
-    if (assignees.length > 0) {
-      core.info("It's not possible to assign a PR already assigned");
-      return;
-    }
-
-    if (type === "Bot") {
-      core.info("It's not possible to assign a PR to a bot");
-      return;
-    }
-  }
-
-  async assignUser() {
-    const {
-      number,
-      user: { login: author },
-    } = this.target;
-
-    const result = await this.octokit.rest.issues.addAssignees({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: number,
-      assignees: [author],
-    });
-
-    core.debug(JSON.stringify(result));
-    core.info(`@${author} has been assigned to the pull request: #${number}`);
-  }
-}
+import { GitHubPullRequest } from "./github.pull.request";
 
 async function run() {
   try {
-    const token = core.getInput("repo-token", { required: true });
-    const githubPullRequest = new GitHubPullRequest(token);
+    const githubPullRequest = new GitHubPullRequest();
     githubPullRequest.validate();
     githubPullRequest.assignUser();
   } catch (error) {
